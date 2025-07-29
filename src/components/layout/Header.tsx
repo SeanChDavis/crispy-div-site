6946f172ba5957e87f99355e5d1c2521b070a1a2
SeanChDavis <seanchdavis@gmail.com>
2025-07-29 07:28:18 -0500

    fix header spacing

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,9 +5,9 @@ import LinkButton from "../elements/LinkButton.tsx";
 export default function Header() {
     return (
         <header className="justify-center bg-zinc-950 text-zinc-400">
-            <div className="max-w-2xl mx-auto px-4 pt-12 sm:pt-16 md:pt-24 lg:pt-32 mb-10 md:mb-14 text-center">
+            <div className="max-w-2xl mx-auto px-4 pt-12 sm:pt-16 md:pt-24 mb-10 md:mb-14 text-center">
                 <div className="max-w-2xl mx-auto text-center mb-14 md:mb-20">
-                    <h1 className="text-xl md:text-2xl lg:text-3xl text-white font-semibold mb-2">{d.siteHeader.title}</h1>
+                    <h1 className="text-2xl md:text-3xl text-white font-semibold mb-2">{d.siteHeader.title}</h1>
                     <p className="text-sm mb-6">
                         {d.siteDetails.author.introText} <a href={d.siteDetails.author.url} className="underline hover:no-underline" target="_blank">{d.siteDetails.author.name}</a>
                     </p>
